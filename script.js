2456d4835ce4467a3a5915529fc7303885b2e860
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2023-10-09 15:37:55 -0500

    used jquery function

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#minimize').on('click', function(){
+        $('.footer').css('foot-size', '7px');
+        $('.footnote').css('foot-size', '10px');
+    })
+})
